a79d1a9e1520a2346f75582b659fc3abc7d37e1e
Adrian <130798996+Mos7ach0@users.noreply.github.com>
2023-07-08 13:21:45 -0600

    Se ha inicializado la ListaSimplementeEnlazada

---
+++ ListaSimplementeEnlazada.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+
+struct Nodo {
+    int dato;
+    Nodo* siguiente;
+};
+
+
+class ListaEnlazada {
+private:
+    Nodo* inicio;
+
+public:
+    ListaEnlazada() {
+        
+    }
+
+
+    void insertarAlInicio(int dato) {
+        
+    }
+
+ 
+    void mostrarLista() {
+        
+    }
+
+
+    void volverNuloPorValor(int valor){
+
+    }
+
+};
+
+int main() {
+}
