266f9350ca9be689e822a45cc95f5c8925253768
Adrian <130798996+Mos7ach0@users.noreply.github.com>
2023-07-15 12:28:22 -0600

    Implementacion de codigo

--- ListaSimplementeEnlazada.cpp
+++ ListaSimplementeEnlazada.cpp
@@ -15,22 +15,46 @@ private:
 public:
     // Sabado 15 de julio
     ListaEnlazada() {
+        inicio = nullptr;
         
     }
 
     // Sabado 15 de julio
     void insertar(int dato) {
-        
+        Nodo* nuevoNodo = new Nodo;
+        nuevoNodo->dato = dato;
+        nuevoNodo->siguiente = inicio;
+        inicio = nuevoNodo;
     }
 
     // Sabado 15 de julio 
     void imprimir() {
-        
+        Nodo* temp = inicio;
+        while (temp != nullptr) {
+            cout << temp->dato << " ";
+            temp = temp->siguiente;
+        }
+        cout << endl;
     }
 
     // volverNuloPorValor es su nombre anterior
     void eliminar(int valor){
+        Nodo* temp = inicio;
+        Nodo* nodoAnterior = nullptr;
 
+        while(temp != nullptr){
+            if(temp->dato == valor){
+                if(nodoAnterior != nullptr){
+                    nodoAnterior->siguiente = temp->siguiente;
+                } else {
+                    inicio = temp->siguiente;
+                }
+                delete temp;
+                break;
+            }
+            nodoAnterior = temp;
+            temp = temp->siguiente;
+        }
     }
 
     int obtener(int valor){
